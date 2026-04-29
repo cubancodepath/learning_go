22efaacc1a1e07e3d5c0f989eda7b9370c26a0d4
Barbaro Javier <barbaro.valmaseda@gmail.com>
2026-04-29 15:59:22 +0400

    feat: add cubancodemath v2 submodule and update mod usage

---
+++ mod_usage/go.mod
@@ -0,0 +1,7 @@
+module mod_usage
+
+go 1.26.1
+
+require github.com/cubancodepath/cubancodemath/v2 v2.0.0
+
+require golang.org/x/exp v0.0.0-20260410095643-746e56fc9e2f // indirect
---
+++ mod_usage/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cubancodepath/cubancodemath/v2"
+)
+
+func main() {
+	result := cubancodemath.Add(3.4, 4.4)
+	fmt.Println(result)
+}
